9fb13b614f37bc3911ebdef7a58c2e95fed56838
eransam <eransam21@gmail.com>
2023-03-06 21:40:08 +0200

    erer

--- my-app/src/app/signup-form/signup-form.component.ts
+++ my-app/src/app/signup-form/signup-form.component.ts
@@ -8,20 +8,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
+    
     //html כאן אנו יוצרים משתנה שמכיל קבוצה של קונטרולרים אשר נמצאים בפורם בקובץ ה
     form = new FormGroup({
         //required אשר הקונטרולרים האלו הם  html כאן אנו יוצרים שמות של פורם קונטרולס שאותם אנו נקשר לשדות בפורם בקובץ ה 
         username:new FormControl('', [
             Validators.required,
             Validators.minLength(3),
-            UsernameValidators.cannotContainSpace    
+            UsernameValidators.cannotContainSpace,
+            UsernameValidators.shouldBeUnique  
         ]),
-        password:new FormControl('', Validators.required)
+        
     });
+
 
     get username(){
         return this.form.get("username")
     }
-
+    
+  
      
 }
--- my-app/src/app/signup-form/username.validators.ts
+++ my-app/src/app/signup-form/username.validators.ts
@@ -2,10 +2,29 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class UsernameValidators {
     static cannotContainSpace(control:AbstractControl) : ValidationErrors | null{
+        console.log("(control.value as string).indexOf" , (control.value as string).indexOf(" ") >=0);
+        
         if ((control.value as string).indexOf(" ") >=0)
         return {cannotContainSpace:true}
 
         return null;
           
         }
+
+
+        static shouldBeUnique(control:AbstractControl) : Promise<ValidationErrors | null>{
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log("control.value: " , control.value);
+                    console.log("control.value === 'mosh' " , control.value === 'mosh');
+                    if (control.value === 'mosh') {
+                        console.log("1");
+                        
+                        resolve ({shouldBeUnique: true});    
+                    }else{
+                        resolve(null);
+                    }
+                }, 2000);
+            });      
+            }
     }
